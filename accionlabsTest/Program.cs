13f8cbd5f7dd83ea063ab664df2de5e166efe2d2
rgupta <rgupta@travelrepublic.com>
2020-07-20 20:42:38 +0100

    commit2

--- accionlabsTest/Program.cs
+++ accionlabsTest/Program.cs
@@ -11,34 +11,36 @@ namespace accionlabsTest
             {
                 Console.WriteLine("Accionlabs Test!");
 
-               
 
-                // Test1_recursion.PrintRecursionValue();
 
-                //Test2_payment_gateways_integrated.Start();
+                Test1_recursion.PrintRecursionValue();
 
-                //Test5_Reverse_string.Start();
+                Test2_payment_gateways_integrated.Start();
+
+                //Test4_HttpPatchVerb
+
+                Test5_Reverse_string.Start();
 
                 // 3) C# Program Implements Arithmetic Operations using Delegates
 
 
-            //    Calculation Add = new Calculation(Test3_ArithmeticOperations.Add);
-            //    Console.WriteLine(Add(5, 6));
+                Calculation Add = new Calculation(Test3_ArithmeticOperations.Add);
+                Console.WriteLine(Add(5, 6));
 
-            //    Calculation Sub = new Calculation(Test3_ArithmeticOperations.Sub);
-            //    Console.WriteLine(Sub(5, 6));
+                Calculation Sub = new Calculation(Test3_ArithmeticOperations.Sub);
+                Console.WriteLine(Sub(5, 6));
 
 
-            //    // callng deligate example 2 
-            //    var empList = new List<Emp>()
-            //{
-            //    new Emp { Id = 1, Name = "test1"},
-            //    new Emp { Id = 2, Name = "test2" }
-            //};
+                //    // callng deligate example 2 
+                var empList = new List<Emp>()
+                {
+                    new Emp { Id = 1, Name = "test1"},
+                    new Emp { Id = 2, Name = "test2" }
+                };
 
-            //    Test3_Deligate.Search(empList, e => e.Id == 1);
-
-            } 
+                var T = Test3_Deligate.Search(empList, e => e.Id == 1);
+                Console.ReadLine();
+            }
         }
     }
 
